486440de9cc6ae22bf3eb4a3603cf87ccf770d1d
Praveen K <yvabzcmyjbkuaqnwgg@nespf.com>
2025-09-22 07:57:29 +0000

    used codegen package to configure sources

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,28 +1,41 @@
 with customers as (
 
-    select * from {{ ref('stg_jaffle_shop__customers') }}
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from raw.jaffle_shop.customers
 
 ),
 
 orders as (
 
-    select * from {{ ref('fct_orders') }}
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from raw.jaffle_shop.orders
+
 ),
 
 customer_orders as (
 
     select
         customer_id,
+
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders,
-        sum(amount) as lifetime_value
+        count(order_id) as number_of_orders
 
     from orders
 
     group by 1
 
 ),
+
 
 final as (
 
@@ -32,8 +45,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        customer_orders.lifetime_value
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers
 
