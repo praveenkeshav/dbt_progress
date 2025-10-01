7b56ad3123edfd52e038153f7867fee92b4c706d
Praveen K <yvabzcmyjbkuaqnwgg@nespf.com>
2025-10-01 11:52:24 +0000

    unit testing performed for stg_orders and stg_customers models

---
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -0,0 +1,27 @@
+with
+
+source as (
+
+    select * from {{ source('jaffle_shop',  'customers') }}
+
+),
+
+staged as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name,
+
+        upper(left(first_name, 1)) 
+            || lower(right(first_name, len(first_name) -1)) 
+            || ' '
+            || upper(left(last_name, 1)) 
+            || lower(right(last_name, len(last_name) -1)) 
+        as full_name
+
+    from source
+
+)
+
+select * from staged
